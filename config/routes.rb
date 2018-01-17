f5a0ab0e304e8ccde56f42d5fdc953f17d2bb1e5
Mandy Tong <mandytong@Mandys-MacBook-Pro.local>
2018-01-17 14:22:37 -0800

    fixed config routes for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   root 'welcome#index'
-=======
->>>>>>> cf0f0f07d4706bfec642e808edadb6596525d0c7
   resources :reviews, only: [:index]
   resources :albums, only: [:index, :show] do
     resources :reviews, only: [:create, :update, :destroy]
